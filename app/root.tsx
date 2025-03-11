f54040ebdeca08acbe6fc6f48245e109df744235
crrmacarse <crrmacarse@gmail.com>
2025-03-12 07:45:16 +0800

    feat: added dynamic pull of sidebar data

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,32 @@
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from './app.css?url';
+
+import { getContacts } from "./data";
+import type { ContactMutation } from './data';
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref }
+];
+
+export const loader = async ()  => {
+  const contacts: ContactMutation[] = await getContacts();
+
+  return { contacts };
+};
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -35,14 +55,36 @@ export default function App() {
           </div>
           <nav>
             <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
+              {contacts.length ? (
+                <ul>
+                  {contacts.map((contact) => (
+                    <li key={contact.id}>
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? (
+                          <span>★</span>
+                        ) : null}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>
+                  <i>No contacts</i>
+                </p>
+              )}
             </ul>
           </nav>
+        </div>
+
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
