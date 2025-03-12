cddc74a1a2731f42177f97ee57beac800ffad674
crrmacarse <crrmacarse@gmail.com>
2025-03-12 08:30:32 +0800

    feat: added search

--- app/root.tsx
+++ app/root.tsx
@@ -9,21 +9,26 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import appStylesHref from './app.css?url';
 
 import { createEmptyContact, getContacts } from "./data";
-import type { ContactMutation } from './data';
+import { useEffect, useState } from "react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref }
 ];
 
-export const loader = async () => {
-  const contacts: ContactMutation[] = await getContacts();
+export const loader = async ({
+  request
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
 
-  return { contacts };
+  return { contacts, q };
 };
 
 export const action = async () => {
@@ -33,8 +38,21 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
+  const [query, setQuery] = useState(q || "");
+
+  useEffect(() => {
+    setQuery(q || "");
+  }, [q])
 
   return (
     <html lang="en">
@@ -48,15 +66,26 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form id="search-form" role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}>
               <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
+                className={searching ? "loading" : ""}
                 name="q"
+                onChange={(e) => {
+                  setQuery(e.currentTarget.value)
+                }}
+                value={query}
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
